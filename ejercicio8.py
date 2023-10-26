0909fcc366d04b5b169c2a989767672101e87a39
Ghosme27 <113557448+Ghosme27@users.noreply.github.com>
2023-10-26 16:43:49 +0200

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,3 +1,4 @@
-b=int(input('Dime un numero del 1 al 10 que quieras saber la tabla de mutlipicacion'))
-for i in range(0,11):
-    print(i*b,end='\n')
+for i in range(1,11):
+    print('Tabla de mutlipicación del',i)
+    for j in range(0,11):
+        print(i*j)
